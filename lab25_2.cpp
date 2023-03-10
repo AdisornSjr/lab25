1b032669424b39c963f628ed52373e50b98dc3aa
fluke <adisorn.sjr@gmail.com>
2023-03-10 17:02:41 +0700

    com

--- lab25_2.cpp
+++ lab25_2.cpp
@@ -22,6 +22,37 @@ int main(){
 	
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
+
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+
+
+	loc = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc,"Narutu");
+
+	line_up.push_front("Prayath");
+
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Tony");
+	line_up.erase(loc);
+
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+
+	
+
+
+	
 	
 	//Write your code here
 	
--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<set>
+#include<list>
 
 using namespace std;
 
 //Write function count() here.
+int count(int *s,int z){
+	set<int> Myset;
+	for(int i = 0;i<z;i++){
+		Myset.insert(s[i]);
+	}
+	return Myset.size();
+	
+
+
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
